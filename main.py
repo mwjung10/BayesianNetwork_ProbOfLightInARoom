708a1c764c6079fe67891f64594d0a5e6a87ac2b
mwjung <mwjung10@gmail.com>
2025-06-04 21:55:46 +0200

    small fix

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from model import build_bayesian_network, ModelInference
+from model import build_bayesian_network, model_inference
 
 if __name__ == "__main__":
     model = build_bayesian_network()
 
     variables = ["Light"]
-    result_no_evidence = ModelInference(model, variables)
+    result_no_evidence = model_inference(model, variables)
     print("Inference without evidence:")
     print(result_no_evidence)
 
     evidence = {"MSTeams": "on"}
-    result_with_evidence = ModelInference(model, variables, evidence)
+    result_with_evidence = model_inference(model, variables, evidence)
     print("\nInference with evidence (MSteams is on):")
     print(result_with_evidence)
-    
